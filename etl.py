34118dc7f266e678e787b26a6145a96528245c35
JimLin <acgsong.tw@yahoo.com.tw>
2016-10-17 15:54:35 +0800

    Refactor check_work and get_actress_id method

--- etl.py
+++ etl.py
@@ -4,12 +4,16 @@
 import requests
 from bs4 import BeautifulSoup as bs
 import re
+from dao import DAO
 
 class ETL:
 
+    def __init__(self):
+        self.dao = DAO()
+
     def check_new_arrive(self):
         count = 0
-        url = "http://www.dmm.co.jp/mono/dvd/-/calendar/=/day=31/"
+        url = "http://www.dmm.co.jp/mono/dvd/-/calendar/=/day=31-31/"
         r = requests.get(url)
         soup = bs(r.text)
         
@@ -23,10 +27,6 @@ class ETL:
             if actress_tag is None or actress_tag.text == "----":
                 continue
 
-            actress = actress_tag.find("a")
-            if actress is None:
-                continue
-
             title_tag  = work.find("td", {"class": "title-monocal"})
             title = title_tag.find("a")
             title_name = title.text
@@ -35,38 +35,26 @@ class ETL:
             if match:
                 continue
 
-            # check_work(title)
-            
             title_url = "http://www.dmm.co.jp" + title.get("href")
-
-            detail = self.get_detail(title_url)
-            if detail is None:
-                continue
-
-            print actress.text + " " + title_name + " " + title_url
+            self.check_work(title_url)
 
             count += 1
 
         print count
 
-    def check_actress(self, tag):
+    def get_actress_id(self, tag):
         url = tag.get("href")
         pattern = re.compile("/mono/dvd/-/list/=/article=actress/id=(.*)/")
         match = pattern.search(url)
         id = match.group(1)
+        actress = self.dao.find_actress_by_id(id)
+        if actress is None:
+            data = {'id': id, 'name': tag.text, 'url': "http://www.dmm.co.jp" + url}
+            self.dao.insert_actress(data)
 
-        return
+        return id
 
-    def check_work(self, tag):
-        url = tag.get("href")
-        pattern = re.compile("/mono/dvd/-/detail/=/cid=(.*)/")
-        match = pattern.search(url)
-        cid = match.group(1)
-        print cid
-
-        return
-
-    def get_detail(self, url):
+    def check_work(self, url):
         r = requests.get(url)
         soup = bs(r.text)
         
@@ -79,11 +67,22 @@ class ETL:
         if a_tag is None:
             return
 
+        pattern = re.compile("/mono/dvd/-/detail/=/cid=(.*)/")
+        match = pattern.search(url)
+        cid = match.group(1)
+        work = self.dao.find_work_by_no(cid)
+        if work is not None:
+            return
+
         performer = soup.find("span", {"id": "performer"})
         performer_a_tag = performer.find_all("a")
+        actresses = list()
         for i in xrange(len(performer_a_tag)):
             actress = performer_a_tag[i]
-            print actress
-            self.check_actress(actress)
+            actresses.append(self.get_actress_id(actress))
+
+        title = soup.find("h1", {"id": "title"})
+        data = {'angels': actresses, 'no': cid, 'title': title.text, 'url': url, 'cover_url': a_tag.get('href')}
+        self.dao.insert_work(data)
 
         return
